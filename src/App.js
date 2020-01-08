ef1c81e670b35bf3f3ed632e30d9b44ca66afef8
Roy Davis <roy.davis@lic.co.nz>
2020-01-08 15:40:41 +1300

    Turned component into a class, got minimap working, started assembling the form

--- src/App.js
+++ src/App.js
@@ -4,25 +4,93 @@ import FeedWedge from "./components/FeedWedge/";
 
 function App() {
 
-    let farm_walk = [{ name: '1', cover: 3500 }, 
-                 { name: '2', cover: 3200 },
-                 { name: '3', cover: 3000 },
-                 { name: '4', cover: 2800 },
+    let farm_walk = [{ name: '56', cover: 2650 },
+                    { name: '33', cover: 2375 },
+                    { name: '35', cover: 2375 },
+                    { name: '55', cover: 2375 },
+                    { name: '26', cover: 2125 },
+                    { name: '30', cover: 2125 },
+                    { name: '16', cover: 2025 },
+                    { name: '23', cover: 1975 },
+                    { name: '40', cover: 1975 },
+                    { name: '15', cover: 1950 },
+                    { name: '18', cover: 1950 },
+                    { name: '13', cover: 1900 },
+                    { name: '19', cover: 1900 },
+                    { name: '22', cover: 1825 },
+                    { name: '21', cover: 1800 },
+                    { name: '32', cover: 1800 },
+                    { name: '41', cover: 1800 },
+                    { name: '6', cover: 1800 },
+                    { name: '11', cover: 1775 },
+                    { name: '17', cover: 1775 },
+                    { name: '20', cover: 1750 },
+                    { name: '12', cover: 1700 },
+                    { name: '38', cover: 1700 },
+                    { name: '45', cover: 1700 },
+                    { name: '46', cover: 1700 },
+                    { name: '49', cover: 1700 },
+                    { name: '51', cover: 1700 },
+                    { name: '28', cover: 1650 },
+                    { name: '24', cover: 1625 },
+                    { name: '57', cover: 1525 },
+                    { name: '34', cover: 1500 },
+                    { name: '42', cover: 1500 },
+                    { name: '54', cover: 1500 },
+                    { name: '1', cover: 1450 },
+                    { name: '36', cover: 1450 },
+                    { name: '53', cover: 1450 },
+                    { name: '37', cover: 1425 },
+                    { name: '48', cover: 1425 },
+                    { name: '27', cover: 1400 },
+                    { name: '43', cover: 1400 },
+                    { name: '25', cover: 1350 },
+                    { name: '31', cover: 1350 },
+                    { name: '47', cover: 1350 },
+                    { name: '5', cover: 1325 },
+                    { name: '39', cover: 1300 },
+                    { name: '10', cover: 1250 },
+                    { name: '3', cover: 1250 },
+                    { name: '50', cover: 1225 },
+                    { name: '4', cover: 1175 },
+                    { name: '9', cover: 1150 },
                  ];
 
     let growth = 100;
-    let pre = 3500;
+    let pre = 2500;
     let post = 1500;
+    let days = 7;
+    let grazing_rate = 2;
 
   return (
     <div className="App">
-  		<header>
-  		</header>
   		<div className="content">
   			<FeedWedge covers={farm_walk} growth={growth} post={post} pre={pre} />
+        <div>
+          <p><input type="checkbox" className="add_target" />Add Pre-grazing target of <input type="number" value={post} /> kg/DM</p>
+          <p>and Post-grazing target of <input type="number" value={pre} /> kg/DM</p>
+        </div>
+        <div>
+          <p><input type="checkbox" className="add_growth" />Add growth <input type="number" value={days} /> days at <input type="number" value={growth} /> kg/DM/day</p>
+        </div>
+        <div>
+          <p><input type="checkbox" className="estimate_grazings" />Estimate paddocks grazed at <input type="number" value={grazing_rate} /><select><option>ha/day</option></select></p>
+        </div>
+        <div>
+
+          <p><input type="checkbox" className="estimate_deficit" />Estimate feed deficits/surplus</p>
+        </div>
   		</div>
+
+    <label class="custom_checkbox">Three
+      <input type="checkbox"  />
+      <span class="checkmark"></span>
+    </label>
+
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/FeedWedge/index.js
+++ src/components/FeedWedge/index.js
@@ -1,125 +1,192 @@
 import React from 'react';
 import './FeedWedge.scss';
 
-const FeedWedge = props => {
 
-	let histogram_container = React.createRef();
+class FeedWedge extends React.Component {
 
-	let histogram_width = 1000;
-	//let histogram_width = props.covers.length * 32;
-	let histogram_bar_width = 24;
-	let histogram_bar_padding = 8;
-	let histogram_bar_spacing = (histogram_bar_padding * 2) + histogram_bar_width;
-	let histogram_viewbox = "0 0 " + histogram_width + " 224";
-	let histogram_residual_bars = [];
-	let histogram_cover_bars = [];
-	let histogram_x = histogram_bar_padding*2;
-	let histogram_residual = props.post/32;
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			histogram_width: 1000,
+			histogram_container_width: 500,
+		};
 
-	let apc = 0;
-
-	for (let i = 0; i < props.covers.length; i++) {
-		apc += props.covers[i].value;
-		let available = (props.covers[i].cover)/32;
-		let avail_y = 192 - available;
-		histogram_cover_bars.push(
-			<rect x={histogram_x} y={avail_y} className="fw_bar_available" width={histogram_bar_width} height={available} key={props.covers[i].name} data-name={props.covers[i].name} data-cover={props.covers[i].cover} />
-		)
-		avail_y = 192 - histogram_residual;
-		histogram_residual_bars.push(
-			<rect x={histogram_x} y={avail_y} className="fw_bar_residual" width={histogram_bar_width} height={histogram_residual} key={props.covers[i].name + "_residual"} data-name={props.covers[i].name} data-cover={props.post} />
-		)
-		histogram_x += histogram_bar_spacing;
+		this.histogram_container = React.createRef();
+		this.sliderController = this.sliderController.bind(this);
 	}
 
-	apc = apc/props.covers.length;
+	componentDidMount(){
 
-	let target_start = 192 - (props.pre/32);
-	let target_end = 192 - (props.post/32);
+		this.setState({
+			histogram_width: this.props.covers.length * 32,
+			histogram_container_width: this.histogram_container.current.offsetWidth,
+		});
+	}
 
-	let target_line = <line className="fw_target" x1="4" y1={target_start} x2={histogram_width} y2={target_end} strokeWidth="2" />
 
-	function sliderController(e) {
+
+
+	sliderController(e) {
 		e.preventDefault();
-		let container = histogram_container.current;
-		let total = container.scrollWidth - container.offsetWidth;
+		let container = this.histogram_container.current;
+		let total = (container.scrollWidth - container.offsetWidth);
 		let percentage = total*(e.target.value/100);
 		container.scrollLeft = percentage;
+
+
 	}
 
+	render() {
 
-  return (
-	<div className="fw_container">
-		<svg	className="y_axis"
-				height="224"
-				width="96"
-				version="1.1" 
-				xmlns="http://www.w3.org/2000/svg" 
-				viewBox="0 0 96 224" >
+		let histogram_bar_width = 24;
+		let histogram_bar_padding = 8;
 
 
-			<line className="st0" x1="96" y1="32" x2="96" y2="192"/>
+		let histogram_residual_bars = [];
+		let histogram_cover_bars = [];
+		let mini_map_cover = [];
+		let mini_map_residual = [];
 
-			<line className="st0" x1="88" y1="32" x2="96" y2="32"/>
-			<line className="st0" x1="88" y1="64" x2="96" y2="64"/>
-			<line className="st0" x1="88" y1="96" x2="96" y2="96"/>
-			<line className="st0" x1="88" y1="128" x2="96" y2="128"/>
-			<line className="st0" x1="88" y1="160" x2="96" y2="160"/>
-			<line className="st0" x1="88" y1="192" x2="96" y2="192"/>
+		
+		let apc_tally = 0;
 
-			<text transform="matrix(1 0 0 1 76.5249 196.6959)" className="st1 st2">0</text>
-			<text transform="matrix(1 0 0 1 52.4108 164.6963)" className="st1 st2">1,000</text>
-			<text transform="matrix(1 0 0 1 50.1496 36.6961)" className="st1 st2">5,000</text>
-			<text transform="matrix(1 0 0 1 50.4479 68.6961)" className="st1 st2">4,000</text>
-			<text transform="matrix(1 0 0 1 50.6427 100.696)" className="st1 st2">3,000</text>
-			<text transform="matrix(1 0 0 1 50.6558 132.6962)" className="st1 st2">2,000</text>
-			<text transform="matrix(6.123234e-17 -1 1 6.123234e-17 26.6456 137.9932)" className="st1 st2">kg/DM/ha</text>
+		
+		let histogram_x = histogram_bar_padding * 2;
+		let	target_start = 192 - (this.props.pre/32);
+		let	target_end = 192 - (this.props.post/32);
+
+		let	histogram_bar_spacing = (histogram_bar_padding * 2) + histogram_bar_width;
 			
+		let	histogram_residual = this.props.post/32;
 
-		</svg>
-		<div className="fw_histogram_container" ref={histogram_container}>
-			<h5>Average Pasture Cover: {apc}kg/Dm/ha</h5>
-			<ul className="fw_legend">
-				<li><div className="fw_legend_residual fw_legend_icon"></div>Residual</li>
-				<li><div className="fw_legend_available fw_legend_icon"></div>Available</li>
-				<li><div className="fw_legend_predicted fw_legend_icon"></div>Predicted Growth</li>
-			</ul>
+		
+		let	minimap_chunks = ((this.state.histogram_container_width - 16 )/this.props.covers.length);
 
-			<svg	className="y_axis"
-					height="224"
-					width={histogram_width}
-					version="1.1" 
-					xmlns="http://www.w3.org/2000/svg" 
-					viewBox={histogram_viewbox} >
-				
-			
-				<line className="st0a" x1="0" y1="32" x2={histogram_width} strokeDasharray="2 2" y2="32"/>
-				<line className="st0a" x1="0" y1="64" x2={histogram_width} strokeDasharray="2 2" y2="64"/>
-				<line className="st0a" x1="0" y1="96" x2={histogram_width} strokeDasharray="2 2" y2="96"/>
-				<line className="st0a" x1="0" y1="128" x2={histogram_width} strokeDasharray="2 2" y2="128"/>
-				<line className="st0a" x1="0" y1="160" x2={histogram_width} strokeDasharray="2 2" y2="160"/>
+		
+		let minimap_spacing = minimap_chunks / 3 ;
+		let minimap_x = 10;
+		let minimap_width = minimap_spacing * 2;
+		
+		let	minimap_residual = (this.props.post)/96;
 
-				<line className="st0" x1={histogram_width} y1="32" x2={histogram_width} y2="192"/>
+		let	slider_handle_width = (this.state.histogram_container_width / histogram_bar_spacing) * (minimap_chunks);
 
-				{histogram_cover_bars}
-				{histogram_residual_bars}
-				{target_line}
 
-				<line className="st0" x1="0" y1="192" x2={histogram_width} y2="192"/>
+		for (let i = 0; i < this.props.covers.length; i++) {
+	
+			// main histogram
+			apc_tally += this.props.covers[i].cover;
+			let available = (this.props.covers[i].cover)/32;
+			let avail_y = 192 - available;
+			histogram_cover_bars.push(
+				<rect x={histogram_x} y={avail_y} className="fw_bar_available" width={histogram_bar_width} height={available} key={this.props.covers[i].name} data-name={this.props.covers[i].name} data-cover={this.props.covers[i].cover} />
+			)
 
-			</svg>
-			<h6 className="st1 st2">Paddocks</h6>
-		</div>
-		<div className="fw_view_slider">
-			<input type="range" min="1" max="100" className="fw_slider" defaultValue="1" onChange={sliderController} />
-			<svg className="fw_mini_view"
-				version="1.1" 
-				xmlns="http://www.w3.org/2000/svg" >
-			</svg>
-		</div>
-	</div>
-	)
-};
+			// residual bars
+			avail_y = 192 - histogram_residual;
+			histogram_residual_bars.push(
+				<rect x={histogram_x} y={avail_y} className="fw_bar_residual" width={histogram_bar_width} height={histogram_residual} key={this.props.covers[i].name + "_residual"} data-name={this.props.covers[i].name} data-cover={this.props.post} />
+			)
+			histogram_x += histogram_bar_spacing;
+
+			// minimap histogram
+			available = (this.props.covers[i].cover)/96;
+			avail_y = 40 - available;
+			mini_map_cover.push(
+				<rect x={minimap_x} y={avail_y} className="fw_bar_available" width={minimap_width} height={available}  data-name={this.props.covers[i].name} data-cover={this.props.covers[i].cover} key={this.props.covers[i].name + "_minimap"} />
+			)
+
+			mini_map_residual.push(
+				<rect x={minimap_x} y={25} className="fw_bar_residual" width={minimap_width} height={minimap_residual}  data-name={this.props.covers[i].name} data-cover={this.props.covers[i].cover} key={this.props.covers[i].name + "_residual_minimap"} />
+			)
+			minimap_x += (minimap_spacing + minimap_width);
+
+		}	
+
+		let apc = apc_tally/this.props.covers.length;
+		let target_line = <line className="fw_target" x1="4" y1={target_start} x2={this.state.histogram_width} y2={target_end} strokeWidth="2" />;
+		let histogram_viewbox= "0 0 " + this.state.histogram_width + " 224";
+
+		return (
+			<div className="fw_container">
+				<svg	className="y_axis"
+						height="224"
+						width="96"
+						version="1.1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 96 224" >
+
+
+					<line className="st0" x1="96" y1="32" x2="96" y2="192"/>
+
+					<line className="st0" x1="88" y1="32" x2="96" y2="32"/>
+					<line className="st0" x1="88" y1="64" x2="96" y2="64"/>
+					<line className="st0" x1="88" y1="96" x2="96" y2="96"/>
+					<line className="st0" x1="88" y1="128" x2="96" y2="128"/>
+					<line className="st0" x1="88" y1="160" x2="96" y2="160"/>
+					<line className="st0" x1="88" y1="192" x2="96" y2="192"/>
+
+					<text transform="matrix(1 0 0 1 76.5249 196.6959)" className="st1 st2">0</text>
+					<text transform="matrix(1 0 0 1 52.4108 164.6963)" className="st1 st2">1,000</text>
+					<text transform="matrix(1 0 0 1 50.1496 36.6961)" className="st1 st2">5,000</text>
+					<text transform="matrix(1 0 0 1 50.4479 68.6961)" className="st1 st2">4,000</text>
+					<text transform="matrix(1 0 0 1 50.6427 100.696)" className="st1 st2">3,000</text>
+					<text transform="matrix(1 0 0 1 50.6558 132.6962)" className="st1 st2">2,000</text>
+					<text transform="matrix(6.123234e-17 -1 1 6.123234e-17 26.6456 137.9932)" className="st1 st2">kg/DM/ha</text>
+					
+
+				</svg>
+				<div className="fw_histogram_container" ref={this.histogram_container}>
+					<h5>Average Pasture Cover: {apc}kg/Dm/ha</h5>
+					<ul className="fw_legend">
+						<li><div className="fw_legend_residual fw_legend_icon"></div>Residual</li>
+						<li><div className="fw_legend_available fw_legend_icon"></div>Available</li>
+						<li><div className="fw_legend_predicted fw_legend_icon"></div>Predicted Growth</li>
+					</ul>
+
+					<svg	className="y_axis"
+							height="224"
+							width={this.state.histogram_width}
+							version="1.1" 
+							xmlns="http://www.w3.org/2000/svg" 
+							viewBox={histogram_viewbox} >
+						
+					
+						<line className="st0a" x1="0" y1="32" x2={this.state.histogram_width} strokeDasharray="2 2" y2="32"/>
+						<line className="st0a" x1="0" y1="64" x2={this.state.histogram_width} strokeDasharray="2 2" y2="64"/>
+						<line className="st0a" x1="0" y1="96" x2={this.state.histogram_width} strokeDasharray="2 2" y2="96"/>
+						<line className="st0a" x1="0" y1="128" x2={this.state.histogram_width} strokeDasharray="2 2" y2="128"/>
+						<line className="st0a" x1="0" y1="160" x2={this.state.histogram_width} strokeDasharray="2 2" y2="160"/>
+
+						<line className="st0" x1={this.state.histogram_width} y1="32" x2={this.state.histogram_width} y2="192"/>
+
+						{histogram_cover_bars}
+						{histogram_residual_bars}
+						{target_line}
+
+						<line className="st0" x1="0" y1="192" x2={this.state.histogram_width} y2="192"/>
+
+					</svg>
+					<h6 className="st1 st2">Paddocks</h6>
+				</div>
+				<div className="fw_minimap">
+					<style dangerouslySetInnerHTML={{
+					  __html: '.fw_slider::-webkit-slider-thumb { width:' + slider_handle_width + 'px;}'
+					  }}>
+					</style>
+					<input type="range" min="0" max="100" className="fw_slider" defaultValue="0" onChange={this.sliderController}  />
+					<svg className="fw_minimap_view"
+						version="1.1" 
+						xmlns="http://www.w3.org/2000/svg" >
+						{mini_map_cover}
+						{mini_map_residual}
+					</svg>
+				</div>
+			</div>
+		)
+	}
+}
 
 
 
